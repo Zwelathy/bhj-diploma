a936c4fff24e1b86dd70294337d413bfd4cf0b95
Zwelathy <tessen666@yandex.ru>
2022-09-27 16:52:27 +0300

    feat: user logic

--- public/js/api/User.js
+++ public/js/api/User.js
@@ -3,13 +3,15 @@
  * регистрацией пользователя из приложения
  * Имеет свойство URL, равное '/user'.
  * */
-class User {
+ class User {
+  static URL = '/user';
+  
   /**
    * Устанавливает текущего пользователя в
    * локальном хранилище.
    * */
   static setCurrent(user) {
-
+    localStorage.setItem(`user`, JSON.stringify(user));
   }
 
   /**
@@ -17,7 +19,7 @@ class User {
    * пользователе из локального хранилища.
    * */
   static unsetCurrent() {
-
+    localStorage.removeItem('user');
   }
 
   /**
@@ -25,7 +27,7 @@ class User {
    * из локального хранилища
    * */
   static current() {
-
+    return localStorage.user ? JSON.parse(localStorage.user) : undefined;
   }
 
   /**
@@ -33,7 +35,24 @@ class User {
    * авторизованном пользователе.
    * */
   static fetch(callback) {
-
+    createRequest({
+      url: this.URL + '/current',
+      method: 'GET',
+      responseType: 'json',
+      callback: (err, response) => {
+        if (response && response.user) {
+          const user = { 
+            name: response.user.name,
+            id: response.user.id
+          }
+          User.setCurrent(user);
+        }
+        else {
+          User.unsetCurrent();
+        } 
+        callback(err, response);
+      }
+    });
   }
 
   /**
@@ -50,7 +69,11 @@ class User {
       data,
       callback: (err, response) => {
         if (response && response.user) {
-          this.setCurrent(response.user);
+          const user = { 
+            name: response.user.name,
+            id: response.user.id
+          }
+          User.setCurrent(user);
         }
         callback(err, response);
       }
@@ -64,7 +87,22 @@ class User {
    * User.setCurrent.
    * */
   static register(data, callback) {
-
+    createRequest({
+      url: this.URL + '/register',
+      method: 'POST',
+      responseType: 'json',
+      data,
+      callback: (err, response) => {
+        if (response && response.user) {
+          const user = { 
+            name: response.user.name,
+            id: response.user.id
+          }
+          User.setCurrent(user);
+        }
+        callback(err, response);
+      }
+    });
   }
 
   /**
@@ -72,6 +110,17 @@ class User {
    * выхода необходимо вызвать метод User.unsetCurrent
    * */
   static logout(callback) {
-
+    createRequest({
+      url: this.URL + '/logout',
+      method: 'POST',
+      responseType: 'json',
+      data: this.current(),
+      callback: (err, response) => {
+        if (response) {
+          User.unsetCurrent();
+        } 
+        callback(err, response);
+      }
+    });
   }
 }
