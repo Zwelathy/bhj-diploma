5993626f61c379498dc15a3adb4ad3a31c6b536d
Zwelathy <tessen666@yandex.ru>
2022-09-27 16:15:14 +0300

    feat: request creating logic

--- public/js/api/createRequest.js
+++ public/js/api/createRequest.js
@@ -3,5 +3,38 @@
  * на сервер.
  * */
 const createRequest = (options = {}) => {
+  if (options) {
+    const xhr = new XMLHttpRequest;
+    let formData = new FormData();
+    let sentURL = options.url;
+    
+    if (options.method !== 'GET') {
+      Object.entries(options.data).forEach(([key, value]) => formData.append(key, value));
+    }
+    else {
+      formData = '';
+      
+      if (!sentURL.includes('/account')) {
+        sentURL += '?';
+        Object.entries(options.data).forEach(([key, value]) => sentURL += `${key}=${value}&`);
+        sentURL = sentURL.slice(0, -1);  
+      }
+    }
 
+    try {
+      xhr.open(options.method, sentURL);
+      xhr.send(formData);       
+    }
+    catch (err) {
+      options.callback(err, null);
+    }
+
+    xhr.responseType = 'json';
+    xhr.addEventListener('readystatechange', function() {
+      if (xhr.status === 200 && xhr.readyState === xhr.DONE) {           
+        options.callback(null, xhr.response);       
+      }
+    });
+
+  }
 };
