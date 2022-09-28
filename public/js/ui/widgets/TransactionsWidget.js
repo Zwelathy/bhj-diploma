4e2dcdeada23493b0c5c03719b6dc144e89f911d
Zwelathy <tessen666@yandex.ru>
2022-09-28 18:28:11 +0300

    feat: transaction  widget logic

--- public/js/ui/widgets/TransactionsWidget.js
+++ public/js/ui/widgets/TransactionsWidget.js
@@ -4,7 +4,7 @@
  * создания нового дохода или расхода
  * */
 
-class TransactionsWidget {
+ class TransactionsWidget {
   /**
    * Устанавливает полученный элемент
    * в свойство element.
@@ -12,7 +12,11 @@ class TransactionsWidget {
    * необходимо выкинуть ошибку.
    * */
   constructor( element ) {
-
+    if (!element) {
+      throw new Error("Невалидное значение для TransactionsWidget"); 
+    }
+    this.element = element;
+    this.registerEvents();
   }
   /**
    * Регистрирует обработчики нажатия на
@@ -21,6 +25,22 @@ class TransactionsWidget {
    * экземпляра окна
    * */
   registerEvents() {
+    const transactionButtons = document.querySelectorAll('.transactions-panel > button');
+    transactionButtons.forEach(button => {
+      button.addEventListener('click', (event) => {
+        event.preventDefault();
 
+        switch (event.target.closest('button').classList[3]) {
+          case 'create-income-button':
+            App.getModal('newIncome').open();
+          break;
+
+          case 'create-expense-button':
+            App.getModal('newExpense').open();
+          break;
+        }
+      });
+    });
   }
+  
 }
