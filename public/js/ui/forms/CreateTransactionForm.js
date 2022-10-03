e28d4694fb5a10923ab34e9bf6928391f8bd3b9d
Zwelathy <tessen666@yandex.ru>
2022-10-03 23:15:43 +0300

    feat: transaction creation logic

--- public/js/ui/forms/CreateTransactionForm.js
+++ public/js/ui/forms/CreateTransactionForm.js
@@ -3,29 +3,57 @@
  * создания новой транзакции
  * */
 class CreateTransactionForm extends AsyncForm {
-  /**
-   * Вызывает родительский конструктор и
-   * метод renderAccountsList
-   * */
-  constructor(element) {
-    super(element)
-  }
+ /**
+  * Вызывает родительский конструктор и
+  * метод renderAccountsList
+  * */
+ constructor(element) {
+   super(element)
+   this.renderAccountsList();
+   this.element = element;
+ }  
 
-  /**
-   * Получает список счетов с помощью Account.list
-   * Обновляет в форме всплывающего окна выпадающий список
-   * */
-  renderAccountsList() {
+ /**
+  * Получает список счетов с помощью Account.list
+  * Обновляет в форме всплывающего окна выпадающий список
+  * */
+ renderAccountsList() {
+   const modaAccList = this.element.querySelector('select.accounts-select');
+   modaAccList.innerHTML = '';
+   const data = User.current();  
+   Account.list(data, (error, response) => { 
+     if (response.success) {
+       response.data.forEach(key => modaAccList.insertAdjacentHTML('beforeend', 
+       `<option value="${key.id}">${key.name}</option>`))
+     }
+   });
+ }
 
-  }
+ /**
+  * Создаёт новую транзакцию (доход или расход)
+  * с помощью Transaction.create. По успешному результату
+  * вызывает App.update(), сбрасывает форму и закрывает окно,
+  * в котором находится форма
+  * */
+ onSubmit(data) {
+   Transaction.create(data, (error, response) => {
+     if (response.success) {
+       let modalMenuName;
 
-  /**
-   * Создаёт новую транзакцию (доход или расход)
-   * с помощью Transaction.create. По успешному результату
-   * вызывает App.update(), сбрасывает форму и закрывает окно,
-   * в котором находится форма
-   * */
-  onSubmit(data) {
+       switch (data.type) {
+         case 'expense':
+          modalMenuName = 'newExpense';
+         break;
 
-  }
+         case 'income':
+          modalMenuName = 'newIncome';
+         break;
+       }
+
+       document.forms[`new-${data.type}-form`].reset();
+       App.getModal(modalMenuName).close();
+       App.update();
+     }
+   });
+ }
 }
